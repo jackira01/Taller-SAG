f0dca8bd3cb79b41bb27d63e3de6a4b099600ea6
jackira01 <s4bu3z0@gmail.com>
2023-04-19 11:26:09 -0500

    add changes

--- ferreteria/api/src/db.js
+++ ferreteria/api/src/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
-const DB =
-  "mongodb+srv://admin:admin@ferreteria.logo8tc.mongodb.net/?retryWrites=true&w=majority";
+const DB = process.env.MONGODB_URI;
+console.log(DB);
 
 mongoose.set("strictQuery", false);
 
