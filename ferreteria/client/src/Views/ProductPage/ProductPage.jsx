71a1499488ff4c11765986569616de046a0da793
jackira01 <s4bu3z0@gmail.com>
2023-05-02 18:55:51 -0500

    add chatbot

--- ferreteria/client/src/Views/ProductPage/ProductPage.jsx
+++ ferreteria/client/src/Views/ProductPage/ProductPage.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import Profile from "../../Components/Auth0/Profile";
 
 const ProductPage = () => {
   return (
     <div>
       <h1>Productos</h1>
+      <Profile />
     </div>
   );
 };
