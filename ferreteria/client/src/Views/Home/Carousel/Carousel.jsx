82dd31b897392cd718c71d74e1281de3684ef666
jackira01 <s4bu3z0@gmail.com>
2023-05-02 18:55:51 -0500

    add chatbot

--- ferreteria/client/src/Views/Home/Carousel/Carousel.jsx
+++ ferreteria/client/src/Views/Home/Carousel/Carousel.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from "react";
+import React from "react";
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
 
@@ -32,10 +32,10 @@ const Carousel = () => {
         className="mySwiper"
       >
         <SwiperSlide>
-          <img src="https://th.bing.com/th/id/R.2ed64a9cadc0301c13ff89dee64c3872?rik=fJcWqhs8DWbX1g&riu=http%3a%2f%2fwww.solofondos.com%2fwp-content%2fuploads%2f2016%2f02%2fFondos-wallpapers-4k.jpg&ehk=Cn%2bGjb7DNAZ3CbZR5hPdsAxUED6i%2bJAPKHBgKmCrjZ8%3d&risl=&pid=ImgRaw&r=0" />
+          <img alt="ejemplo" src="https://th.bing.com/th/id/R.2ed64a9cadc0301c13ff89dee64c3872?rik=fJcWqhs8DWbX1g&riu=http%3a%2f%2fwww.solofondos.com%2fwp-content%2fuploads%2f2016%2f02%2fFondos-wallpapers-4k.jpg&ehk=Cn%2bGjb7DNAZ3CbZR5hPdsAxUED6i%2bJAPKHBgKmCrjZ8%3d&risl=&pid=ImgRaw&r=0" />
         </SwiperSlide>
         <SwiperSlide>
-          <img src="https://th.bing.com/th/id/R.a7089cebf1b1ef32300d9ef81b72f4c0?rik=BAdLIE2cwMeOcQ&riu=http%3a%2f%2fwww.solofondos.com%2fwp-content%2fuploads%2f2015%2f12%2fFondos-de-pantalla-4K.jpg&ehk=RaD5kIcNOaMIFL%2bMTtzDnVxh5PraACmhyduj87aU1%2fQ%3d&risl=&pid=ImgRaw&r=0" />
+          <img alt="ejemplo" src="https://th.bing.com/th/id/R.a7089cebf1b1ef32300d9ef81b72f4c0?rik=BAdLIE2cwMeOcQ&riu=http%3a%2f%2fwww.solofondos.com%2fwp-content%2fuploads%2f2015%2f12%2fFondos-de-pantalla-4K.jpg&ehk=RaD5kIcNOaMIFL%2bMTtzDnVxh5PraACmhyduj87aU1%2fQ%3d&risl=&pid=ImgRaw&r=0" />
         </SwiperSlide>
       </Swiper>
     </>
--- ferreteria/client/src/index.js
+++ ferreteria/client/src/index.js
@@ -1,12 +1,10 @@
+import "./assets/main.css";
+
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import "./index.css";
-import "./assets/main.css";
 import App from "./App.jsx";
+import { Auth0Provider } from "@auth0/auth0-react";
+const { REACT_APP_CLIENT_ID_AUTH0, REACT_APP_DOMAIN_AUTH0 } = process.env;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<App />);
