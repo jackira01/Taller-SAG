126715d1893ad3fa3af125d1230856383e21ad6f
jackira01 <s4bu3z0@gmail.com>
2023-05-05 01:34:54 -0500

    create controllers

--- ferreteria/api/src/controllers/products/products.get.js
+++ ferreteria/api/src/controllers/products/products.get.js
@@ -2,8 +2,23 @@ const Product = require("../../schemas/Products");
 
 const productsCtrl = {};
 
-productsCtrl.getProducts = async (req, res) => {};
+productsCtrl.getProducts = async (req, res) => {
+  const allProducts = await Product.find();
+  try {
+    res.status(200).send(allProducts);
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+};
 
-productsCtrl.productById = async (req, res) => {};
+productsCtrl.productById = async (req, res) => {
+  const { id } = req.params;
+  const productById = await Product.findById(id);
+  try {
+    res.status(200).send(productById);
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+};
 
 module.exports = productsCtrl;
