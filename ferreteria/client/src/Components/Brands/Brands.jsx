dec072bd1348b26308e8972f4434e4b0f75d7d59
jackira01 <s4bu3z0@gmail.com>
2023-05-10 10:22:04 -0500

    fixed errors

--- ferreteria/client/src/Components/Brands/Brands.jsx
+++ ferreteria/client/src/Components/Brands/Brands.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Brands = () => {
   return (
-    <div className="bg-white py-24 sm:py-32">
+    <div className="py-24 sm:py-32">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <h2 className="text-center text-lg font-semibold leading-8 text-gray-900">
           Encuentra Tu Marca Preferida!
